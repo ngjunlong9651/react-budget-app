46034a234c4ebb95ffe882fc910885518ef78d26
Ng Jun Long <42053960+ngjunlong9651@users.noreply.github.com>
2023-11-23 06:54:07 +0000

    Revert

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -13,18 +13,13 @@ const AppReducer = (state, action) => {
 			return {
 				...state,
 				budget: action.payload
-			};
+			}
 		case 'Delete_Expense':
 			return {
 				...state,
 				expenses: state.expenses.filter(
 					(expense) => expense.id !== action.payload
 				),
-			};
-		case 'Add_Category':
-			return {
-				...state,
-				categories: [...state.categories, action.payload],
 			}
 		default:
 			return state;
@@ -34,7 +29,6 @@ const AppReducer = (state, action) => {
 const initialState = {
 	budget: 0,
 	expenses: [],
-    categories: ['Food', 'Transport', 'Rent', 'Utilities', 'Entertainment', 'Healthcare']
 };
 
 export const AppContext = createContext();
