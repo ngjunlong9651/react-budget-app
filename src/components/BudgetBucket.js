0cfdf030467e952045d82c03b290b6eed5d45ca2
Ng Jun Long <42053960+ngjunlong9651@users.noreply.github.com>
2023-11-28 07:39:58 +0000

    test

--- src/components/BudgetBucket.js
+++ src/components/BudgetBucket.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 
 function splitBudget(totalBudget) {
   const needs = totalBudget * 0.5;
@@ -7,11 +8,59 @@ function splitBudget(totalBudget) {
   return {
     needs,
     wants,
-    savings
+    savings,
   };
 }
 
-// Example usage:
-const totalBudget = 1000;
+const totalBudget = 1000; // Replace with user input
+
 const budgetSplit = splitBudget(totalBudget);
 console.log(budgetSplit);
+
+function BudgetBucket() {
+    const [totalBudget, setTotalBudget] = useState(0);
+    const [budgetSplit, setBudgetSplit] = useState(null);
+
+    const handleBudgetChange = (event) => {
+        const budget = parseFloat(event.target.value);
+        setTotalBudget(budget);
+
+        const split = splitBudget(budget);
+        setBudgetSplit(split);
+    };
+
+    const splitBudget = (totalBudget) => {
+        const needs = totalBudget * 0.5;
+        const wants = totalBudget * 0.3;
+        const savings = totalBudget * 0.2;
+
+        return {
+            needs,
+            wants,
+            savings,
+        };
+    };
+
+    return (
+        <div>
+            <label htmlFor="budgetInput">Enter Total Budget:</label>
+            <input
+                type="number"
+                id="budgetInput"
+                value={totalBudget}
+                onChange={handleBudgetChange}
+            />
+
+            {budgetSplit && (
+                <div>
+                    <h2>Budget Split:</h2>
+                    <p>Needs: {budgetSplit.needs}</p>
+                    <p>Wants: {budgetSplit.wants}</p>
+                    <p>Savings: {budgetSplit.savings}</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default BudgetBucket;
