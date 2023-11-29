0a1c32bda6b5251ee974ca41bf718b56d3415186
Ng Jun Long <42053960+ngjunlong9651@users.noreply.github.com>
2023-11-29 09:56:07 +0000

    initializing db instance

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, initializeFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,5 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const db = getFirestore(app);
 export const auth = getAuth(app)
 export default app;
